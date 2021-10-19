059994e54daa244f63c7a6950b2ac4291a4ab840
sibiraj-s <dev.sibiraj@outlook.com>
2021-10-19 09:21:18 +0530

    Minify umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
+import { terser } from 'rollup-plugin-terser';
 
 /** @type {import("rollup").RollupOptions[] } */
 const config = [
@@ -20,6 +21,23 @@ const config = [
       nodeResolve({
         preferBuiltins: false
       })
+    ]
+  },
+  {
+    input: 'src/htmlminifier.js',
+    output: [{
+      file: 'dist/htmlminifier.umd.bundle.min.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'HTMLMinifier'
+    }],
+    plugins: [
+      commonjs(),
+      nodePolyfills(),
+      nodeResolve({
+        preferBuiltins: false
+      }),
+      terser()
     ]
   },
   {

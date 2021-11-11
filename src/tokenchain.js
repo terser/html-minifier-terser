d9711a677c24facdcaae7543f81e3d82249fa458
sibiraj-s <dev.sibiraj@outlook.com>
2021-11-11 22:05:35 +0530

    Cleaning up tokenizer

--- src/tokenchain.js
+++ src/tokenchain.js
@@ -1,61 +1,60 @@
 'use strict';
 
-function Sorter() {
-}
+class Sorter {
+  sort(tokens, fromIndex = 0) {
+    for (let i = 0, len = this.keys.length; i < len; i++) {
+      const key = this.keys[i];
+      const token = key.slice(1);
 
-Sorter.prototype.sort = function (tokens, fromIndex) {
-  fromIndex = fromIndex || 0;
-  for (let i = 0, len = this.keys.length; i < len; i++) {
-    const key = this.keys[i];
-    const token = key.slice(1);
-    let index = tokens.indexOf(token, fromIndex);
-    if (index !== -1) {
-      do {
-        if (index !== fromIndex) {
-          tokens.splice(index, 1);
-          tokens.splice(fromIndex, 0, token);
-        }
-        fromIndex++;
-      } while ((index = tokens.indexOf(token, fromIndex)) !== -1);
-      return this[key].sort(tokens, fromIndex);
-    }
-  }
-  return tokens;
-};
+      let index = tokens.indexOf(token, fromIndex);
 
-function TokenChain() {
-}
+      if (index !== -1) {
+        do {
+          if (index !== fromIndex) {
+            tokens.splice(index, 1);
+            tokens.splice(fromIndex, 0, token);
+          }
+          fromIndex++;
+        } while ((index = tokens.indexOf(token, fromIndex)) !== -1);
 
-TokenChain.prototype = {
-  add: function (tokens) {
-    const self = this;
-    tokens.forEach(function (token) {
-      const key = '$' + token;
-      if (!self[key]) {
-        self[key] = [];
-        self[key].processed = 0;
+        return this[key].sort(tokens, fromIndex);
       }
-      self[key].push(tokens);
+    }
+    return tokens;
+  }
+}
+
+class TokenChain {
+  add(tokens) {
+    tokens.forEach((token) => {
+      const key = '$' + token;
+      if (!this[key]) {
+        this[key] = [];
+        this[key].processed = 0;
+      }
+      this[key].push(tokens);
     });
-  },
-  createSorter: function () {
-    const self = this;
+  }
+
+  createSorter() {
     const sorter = new Sorter();
-    sorter.keys = Object.keys(self).sort(function (j, k) {
-      const m = self[j].length;
-      const n = self[k].length;
+
+    sorter.keys = Object.keys(this).sort((j, k) => {
+      const m = this[j].length;
+      const n = this[k].length;
       return m < n ? 1 : m > n ? -1 : j < k ? -1 : j > k ? 1 : 0;
-    }).filter(function (key) {
-      if (self[key].processed < self[key].length) {
+    }).filter((key) => {
+      if (this[key].processed < this[key].length) {
         const token = key.slice(1);
         const chain = new TokenChain();
-        self[key].forEach(function (tokens) {
+
+        this[key].forEach((tokens) => {
           let index;
           while ((index = tokens.indexOf(token)) !== -1) {
             tokens.splice(index, 1);
           }
-          tokens.forEach(function (token) {
-            self['$' + token].processed++;
+          tokens.forEach((token) => {
+            this['$' + token].processed++;
           });
           chain.add(tokens.slice(0));
         });
@@ -66,6 +65,6 @@ TokenChain.prototype = {
     });
     return sorter;
   }
-};
+}
 
 module.exports = TokenChain;
